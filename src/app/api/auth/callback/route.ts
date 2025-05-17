491aa64c765cb780ae05ef2dcebd3e49a7fd3d99
Manassés Cahunda  (6) <150891445+manassesCahunda@users.noreply.github.com>
2025-05-17 08:25:18 +0100

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,34 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { serialize } from 'cookie';
+import { cookies } from 'next/headers';
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const token = url.searchParams.get('token');
 
   if (!token) {
-    return new NextResponse(
-      JSON.stringify({ error: 'TOKEN is required' }),
-      {
-        status: 400,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
+    return NextResponse.json({ error: 'TOKEN is required' }, { status: 400 });
   }
 
   const redirectUrl = process.env.WEB ? `${process.env.WEB}/chat` : '/';
 
-  const cookieOptions = {
-    sameSite: 'lax' as const,
-    path: '/',
-    maxAge: 60 * 60 * 24 * 7,
-  };
-
-  const cookieToken = serialize('token', token, cookieOptions);
-
-  
   const response = NextResponse.redirect(redirectUrl);
-
-  response.headers.append('Set-Cookie', cookieToken);
+  
+  response.cookies.set({
+    name: 'token',
+    value: token,
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    sameSite: 'lax',
+  });
 
   return response;
 }
