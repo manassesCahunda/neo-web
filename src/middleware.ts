3244be96c51cfb9aadb1715415c1a1b7d0628b6f
Manassés Cahunda  (6) <150891445+manassesCahunda@users.noreply.github.com>
2025-05-18 11:07:51 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,12 @@ import { select } from "@/action/user/select";
 export async function middleware(req: NextRequest) {
   const session = await sessionStore({req}); 
   const publicRoutes = ['/login'];
-  const result = await select();
-  if (!session && !publicRoutes.includes(req.nextUrl.pathname))  return NextResponse.redirect(new URL("/login", req.url));
-  if (session) {
-    if(publicRoutes.includes(req.nextUrl.pathname)) return NextResponse.redirect(new URL("/", req.url));
-    if(result?.message?.prompt?.trim() === '' &&  req.nextUrl.pathname !== '/form' ) return NextResponse.redirect(new URL("/form", req.url)); 
-  }
+  // const result = await select();
+  // if (!session && !publicRoutes.includes(req.nextUrl.pathname))  return NextResponse.redirect(new URL("/login", req.url));
+  // if (session) {
+  //   if(publicRoutes.includes(req.nextUrl.pathname)) return NextResponse.redirect(new URL("/", req.url));
+  //   if(result?.message?.prompt?.trim() === '' &&  req.nextUrl.pathname !== '/form' ) return NextResponse.redirect(new URL("/form", req.url)); 
+  // }
   
   return NextResponse.next();
 }
